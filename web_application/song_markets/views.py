cef2556de14ac1e16b45fe4b0159bcff622733a7
kostyantin <kostyantin1408@gmail.com>
2023-02-23 17:59:59 +0200

    added search form

--- web_application/song_markets/views.py
+++ web_application/song_markets/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def main(request):
-    return render(request, 'song_markets/index.html', {})
+    """
+    Function
+    """
+    if request.method == 'POST':
+        artist = request.POST.get('artist-name')
+        return render(request, 'song_markets/search.html', {"artist": artist})
+    return render(request, 'song_markets/main.html', {})
